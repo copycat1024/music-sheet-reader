988d1234045b18801d3f8d5d4c4e6dc3c384f64a
copycat1024 <tranminhhien102408@gmail.com>
2017-11-09 21:37:26 +0200

    Split edit

---
+++ libs/cc_music_transform.hh
@@ -0,0 +1,21 @@
+#ifndef CC_MUSIC_EDIT
+#define CC_MUSIC_EDIT
+
+#include <opencv2/core/core.hpp>
+#include <string>
+#include <vector>
+
+namespace cc{
+
+void drawLines(cv::Mat, std::vector<cv::Vec4i>, cv::Scalar);
+void drawRects(cv::Mat, std::vector<cv::Vec4i>, cv::Scalar);
+void applyMorphFilter(cv::Mat, int, int);
+void applyMorphFilter2(cv::Mat, int);
+void removeAllLines(cv::Mat, std::vector<cv::Vec4i>);
+cv::Mat polarize(cv::Mat);
+void inverse(cv::Mat);
+std::vector<cv::Mat> splitFrames(cv::Mat, std::vector<cv::Vec4i>);
+
+}
+
+#endif
